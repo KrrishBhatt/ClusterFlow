af4dedddf65ff93b4009304986f22297bf75a52a
krrishbhatt <krrishbhatt2005@gmail.com>
2026-04-06 00:18:07 +0530

    Fixed some bugs

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.'
-import './index'
-
+import App from './App'
+import './services/index'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
